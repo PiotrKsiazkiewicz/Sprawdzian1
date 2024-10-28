29268c09c8951ea7c8e0378fe2c93302d1b419fb
Dante-bot-beep <Piotr.Ksiazkiewicz2008@interia.pl>
2024-10-28 11:12:50 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,6 @@ public class Main {
         System.out.println("Podaj poziom klasy (np. 2, bo 2BT)");
         int level = scanner.nextInt();
         System.out.println("Witaj " + name + ". Za " + (5 - level) + " lata będę pisał maturę !");
-//        na branchu 'twoje imię' w następnej linii wydrukuj co chcesz robić po zakończeniu szkoły średniej
+        System.out.println("Po szkole bede hazardzista");
     }
 }
